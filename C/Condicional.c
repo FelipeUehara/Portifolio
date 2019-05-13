38e88f21a5879c491d036f5c454e5e6a7b5214a8
FelipeUehara <uehara_felipe@hotmail.com>
2019-05-13 11:05:15 -0300

    Criacao do Condicional.c

--- C/Condicional.c
+++ C/Condicional.c
@@ -1,5 +1,40 @@
+//Inicializar as bibliotecas
 #include<stdio.h>
 
 void main(){
+    //Inicializar as varíaveis
+    int idade;
+    
+    //Aquisição de dados
+    printf("Quantos anos voce tem?\n");
+    scanf("%d",&idade);
+
+    //IF ELSE
+    if(idade >= 18){
+        printf("Uau ! Voce ja e maior de idade (>=18)\n");
+    }
+    else{
+        printf("Voce ainda nao e maior de idade(<18)\n");
+    }
+
+    //IF ELSEIF
+    if((idade >=18)&&(idade <65)){
+        printf("Voce pode trabalhar(>18 e <65)\n");
+    }
+    else if(idade > 65){
+        printf("Voce pode se aposentar (>18 e >65)!\n");
+    }
+    
+    //SWITCH CASE DEFAULT
+    switch(idade){
+        case 10:
+            printf("Voce tem uma dezena de anos (10)!\n");
+            break;
+        case 70:
+            printf("Voce tem sete dezenas de anos (70)!\n");
+            break;
+        default:
+            printf("Nao sei quantas dezenas de anos voce tem (-10 e -70!)\n");
+    }
 
 }
--- C/LeituraEscrita.c
+++ C/LeituraEscrita.c
@@ -1,10 +1,17 @@
+//Escrever no terminal, ler dados do usuário, armazenar e mostrar para o usuário.
+//Inicializar librarys
 #include<stdio.h>
 
 void main(){
+    //Inicializar variáveis
     char nome[20];
+    int idade;
 
+    //Escrita //Leitura //Escrita com dados da leitura //Mecher com os dados do usuário
     printf("Ola Mundo ! Qual seu nome?\n");
     scanf("%s",&nome);
-    printf("\nMuito prazer %s",nome);
+    printf("Muito prazer %s qual a sua idade?\n",nome);
+    scanf("%d",&idade);
+    printf("Eu sou 10 anos mais velho, entao eu tenho %d anos\n",idade+10);
 
 }
