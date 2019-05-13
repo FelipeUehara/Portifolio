cd98e49e1ffe76a9e8592beab700044a21318fe4
FelipeUehara <uehara_felipe@hotmail.com>
2019-05-13 11:32:01 -0300

    nome denovo

--- C/3-Repeticao.c
+++ C/3-Repeticao.c
@@ -1,16 +1,20 @@
-//Contagem
+//Contagem utilizando lacos de repeticao
+//Incializacao das bibliotecas
 #include<stdio.h>
 
 void main(){
+    //Inicializacao das variaveis
     int data;
     int x;
     int y;
 
+    //for
     printf("Contagem de 0 a 9\n");
     for(data = 0; data < 10; data ++){
         printf("%d ",data);
     }
 
+    //for for
     printf("\n\nContagem de 0 a 99 pulando linha a cada 10 numeros\n");
     for(y = 0; y < 10; y++){
         for(x = 0; x < 10; x++){
@@ -20,6 +24,7 @@ void main(){
         printf("\n");
     }
 
+    //while
     printf("\nContagem ate o valor indicado\n");
     scanf("%d",&data);
     printf("\n");
